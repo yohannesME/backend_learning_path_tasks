8f49c38a38e2d923ac0a07f53e59e781afa930b5
YohannesME <yohannesmesg@gmail.com>
2023-08-01 11:38:17 -0700

    added the task 2 into the github

---
+++ task_2_word_frequency_count/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = Console.ReadLine();
+
+// case insensetive
+char[] words= input.ToLower().ToCharArray();
+
+// remove non letters
+Dictionary<char, int> freqCount = new Dictionary<char, int>();
+for (int i = 0; i < words.Length; i++)
+{
+    if (char.IsLetter(words[i]))
+    {
+        if(freqCount.ContainsKey(words[i]))
+            freqCount[words[i]]++;
+        else{
+            freqCount.Add(words[i], 1);
+        }
+    }
+}
+
+// Each Input Count
+Console.WriteLine("Word Count");
+foreach (var item in freqCount)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
