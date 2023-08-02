1fa3a6334d3547320a39c2d863156949633243be
YohannesME <yohannesmesg@gmail.com>
2023-08-02 00:14:56 -0700

    made the task two modular

--- task_2_word_frequency_count/Program.cs
+++ task_2_word_frequency_count/Program.cs
@@ -1,26 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string input = Console.ReadLine();
+string inp = Console.ReadLine();
 
-// case insensetive
-char[] words= input.ToLower().ToCharArray();
-
-// remove non letters
-Dictionary<char, int> freqCount = new Dictionary<char, int>();
-for (int i = 0; i < words.Length; i++)
+Dictionary<char, int> freqCounter(string input)
 {
-    if (char.IsLetter(words[i]))
+
+    // case insensetive
+    char[] words = input.ToLower().ToCharArray();
+
+    // remove non letters
+    Dictionary<char, int> freqCount = new Dictionary<char, int>();
+    for (int i = 0; i < words.Length; i++)
     {
-        if(freqCount.ContainsKey(words[i]))
-            freqCount[words[i]]++;
-        else{
-            freqCount.Add(words[i], 1);
+        if (char.IsLetter(words[i]))
+        {
+            if (freqCount.ContainsKey(words[i]))
+                freqCount[words[i]]++;
+            else
+            {
+                freqCount.Add(words[i], 1);
+            }
         }
     }
+    return freqCount;
 }
 
+Dictionary<char, int> freqCount = freqCounter(inp);
 // Each Input Count
 Console.WriteLine("Word Count");
 foreach (var item in freqCount)
 {
     Console.WriteLine($"{item.Key}: {item.Value}");
 }
+
